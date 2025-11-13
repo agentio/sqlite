7383a4a9ee2b98fbd0bb8461144b1fed86d8db0c
Tim Burks <timburks@me.com>
2025-11-13 12:26:04 -0800

    sqlitex: add a simple ExecuteTransient function for backward-compatibility.

---
+++ sqlitex/sqlitex.go
@@ -0,0 +1,47 @@
+package sqlitex
+
+import (
+	"fmt"
+
+	"github.com/agentio/sqlite/sqliteh"
+	"github.com/agentio/sqlite/sqlitepool"
+)
+
+type ExecOptions struct {
+	Args       []any
+	ResultFunc func(stmt sqliteh.Stmt) error
+}
+
+func ExecuteTransient(conn *sqlitepool.Rx, query string, options *ExecOptions) error {
+	stmt := conn.Prepare(query)
+	for i, a := range options.Args {
+		switch v := a.(type) {
+		case nil:
+			stmt.BindNull(i + 1)
+		case int64:
+			stmt.BindInt64(i+1, v)
+		case float64:
+			stmt.BindDouble(i+1, v)
+		case string:
+			stmt.BindText64(i+1, v)
+		case []byte:
+			stmt.BindBlob64(i, v)
+		default:
+			return fmt.Errorf("unhandled type %T (fixme)", a)
+		}
+	}
+	running := true
+	for running {
+		if row, err := stmt.Step(nil); err != nil {
+			return err
+		} else if !row {
+			running = false
+		} else if options.ResultFunc != nil {
+			err = options.ResultFunc(stmt)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return stmt.Reset()
+}
